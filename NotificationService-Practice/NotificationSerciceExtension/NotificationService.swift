1882ac7f2bf7dd4c8092eff8dfee4b1e33ca87ca
ParkSY0919 <qhr498@gmail.com>
2025-10-04 16:16:10 +0900

    [Feat] NotificationServiceExtension 구현
    이는 push notification이 띄워지기 전에 가로채어 특정 작업 수행 가능

---
+++ NotificationService-Practice/NotificationSerciceExtension/NotificationService.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationService.swift
+//  NotificationSerciceExtension
+//
+//  Created by 박신영 on 10/4/25.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+    
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    
+    // APNs를 수신하면 didReceive 메소드 호출
+    // contentHnadler 클로저를 수행하면 푸시가 노출
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        guard let bestAttemptContent else { return }
+        
+        bestAttemptContent.title = "변경 " + request.content.title
+        bestAttemptContent.subtitle = "변경 " + request.content.subtitle
+        contentHandler(bestAttemptContent)
+    }
+    
+    // didReceive에서 contentHandler가 호출되지 않고 특정 시간이 경과하면 이 메소드가 호출
+    override func serviceExtensionTimeWillExpire() {
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+}
