44e38733c99a6dfee0d7a00952ee0bc5e9d439f7
ParkSY0919 <qhr498@gmail.com>
2025-10-04 14:46:00 +0900

    [Setting] Init Project

---
+++ NotificationService-Practice/NotificationService-Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationService-Practice
+//
+//  Created by 박신영 on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationService-Practice/NotificationService-Practice/NotificationService_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationService_PracticeApp.swift
+//  NotificationService-Practice
+//
+//  Created by 박신영 on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationService_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
